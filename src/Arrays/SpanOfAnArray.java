781dcdb2ef22eb24630a6ddbb813ff1b4f734b74
Tanvi Gupta <tanvigupta0129@gmail.com>
2024-08-19 07:12:51 +0530

    Array

---
+++ src/Arrays/SpanOfAnArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SpanOfAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        int span = max - min;
+        System.out.println(span);
+    }
+}
+
+// input: 6, 15, 30, 40, 4, 11, 9
+//output: 36
